f48afbde1ec937bcbeb4bb3d6506fa16ea299a3e
Laurent Thévenoux <laurent.thevenoux@gmail.com>
2017-10-13 13:00:35 +0200

    Improve tests

--- test.ml
+++ test.ml
@@ -1,9 +1,27 @@
-open Mlmpfr;;
-  
-let _ =
+open Mlmpfr
+open Printf
+
+let t_init2 _ =
   let x = mpfr_init2 500 in
   let s = mpfr_set_d x 0.3 MPFR_RNDN in
-  let n = mpfr_out_str "out.txt" 10 12 x MPFR_RNDN in
+  if s <> 0 then printf "Fail while set_d\n";
+  let n = mpfr_out_str "t_init2.txt" 10 100 x MPFR_RNDN in
   mpfr_clear x;
-  print_endline ((string_of_int n) ^ " " ^ (string_of_int s))
+  printf "Write %d characters\n" n
 ;;
+
+let t_inits2 _ =
+  let x = mpfr_inits2 500 5 in
+  printf "Created list size: %d\n" (List.length x);
+  List.iteri (fun i j ->
+      let _ = mpfr_set_d j ((float_of_int i) *. 0.1) MPFR_RNDN in
+      let _ = mpfr_out_str "t_inits2.txt" 10 100 j MPFR_RNDN in
+      let c = open_out_gen [Open_append] 0o640 "t_inits2.txt" in
+      fprintf c "\n";
+      close_out c
+    ) x
+;;
+
+let _ =
+  t_init2 ();
+  t_inits2 ()
