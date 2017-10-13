9f39598676a76429f424de171b5fa3435d743f73
Laurent Thévenoux <laurent.thevenoux@gmail.com>
2017-10-13 17:52:29 +0200

    Support NaN and Inf values in mpfr_out_str

--- mlmpfr.ml
+++ mlmpfr.ml
@@ -27,18 +27,27 @@ external mpfr_get_str : int -> int -> mpfr_t -> rounding_mode -> string * int = 
 
 (* 5.8 Input and Output Functions *)
 let mpfr_out_str chan base n op rnd =
-  let significant, exponent = mpfr_get_str base n op rnd in (* do not forget to test special outputs *)
-  if significant.[0] == '-'
+  let significant, exponent = mpfr_get_str base n op rnd in
+  if String.contains significant '@'
   then
-    Printf.fprintf chan "-%c.%s%c%d"
-                   significant.[1]
-                   (String.sub significant 2 (String.length significant - 2))
-                   (if base > 10 then '@' else 'e')
-                   (exponent - 1)
+    begin
+      Printf.fprintf chan "%s" significant;
+      String.length significant
+    end
   else
-    Printf.fprintf chan "%c.%s%c%d"
-                   significant.[0]
-                   (String.sub significant 1 (String.length significant - 1))
-                   (if base > 10 then '@' else 'e')
-                   (exponent - 1);
-  (String.length significant) + (String.length (string_of_int (exponent -1))) + 2
+    begin
+      if significant.[0] == '-'
+      then
+        Printf.fprintf chan "-%c.%s%c%d"
+                       significant.[1]
+                       (String.sub significant 2 (String.length significant - 2))
+                       (if base > 10 then '@' else 'e')
+                       (exponent - 1)
+      else
+        Printf.fprintf chan "%c.%s%c%d"
+                       significant.[0]
+                       (String.sub significant 1 (String.length significant - 1))
+                       (if base > 10 then '@' else 'e')
+                       (exponent - 1);
+      (String.length significant) + (String.length (string_of_int (exponent -1))) + 2
+    end
