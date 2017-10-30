05960c7bc7a48dbad538354ae536c2e3478c5496
Laurent Thévenoux <laurent.thevenoux@gmail.com>
2017-10-30 14:45:43 +0100

    Fix memory handling

--- testsuite/initializationfunctions.c
+++ testsuite/initializationfunctions.c
@@ -1,21 +1,24 @@
 #include <stdlib.h>
+#include <string.h>
 #include <stdio.h>
 #include <mpfr.h>
 
-void rounding_to_string (char **buf, int r)
+char *rounding_to_string (int r)
 {
+  char * buf = malloc (32);
   if (r == 0)
     {
-      *buf = "Correct\0";
+      strcpy (buf, "Correct");
     }
   else if ( r < 0)
     {
-      *buf = "Lower\0";
+      strcpy (buf, "Lower");
     }
   else
     {
-      *buf = "Higher\0";
+      strcpy (buf, "Higher");
     }
+  return buf;
 }
 
 int main()
@@ -23,25 +26,29 @@ int main()
   mpfr_set_default_prec (12345);
   printf ("default_precision: %ld\n", mpfr_get_default_prec ());
 
-  char *buffer = malloc (128 * sizeof(char));
+  char* buffer;
   mpfr_t one, x;
   mpfr_inits (one, x, NULL);
 
   int r = mpfr_set_si (one, 1, MPFR_RNDN);
-  rounding_to_string (&buffer, r);
+  buffer = rounding_to_string (r);
   printf ("%s\n", buffer);
+  free (buffer);
 
   r = mpfr_set (x, one, MPFR_RNDN);
-  rounding_to_string (&buffer, r);
+  buffer = rounding_to_string (r);
   printf ("%s\n", buffer);
+  free (buffer);
 
   r = mpfr_set_d (x, 1., MPFR_RNDN);
-  rounding_to_string (&buffer, r);
+  buffer = rounding_to_string (r);
   printf ("%s\n", buffer);
+  free (buffer);
 
   r = mpfr_set_str (x, "1", 0, MPFR_RNDN);
-  rounding_to_string (&buffer, r);
+  buffer = rounding_to_string (r);
   printf ("%s\n", buffer);
+  free (buffer);
 
   mpfr_set_nan (x);
   printf ("No_Rounding\n");
@@ -54,7 +61,7 @@ int main()
 
   printf ("%ld\n", mpfr_get_prec (x));
 
-  //free (buffer);
+
   mpfr_clears (one, x, NULL);
   return 0;
 }
