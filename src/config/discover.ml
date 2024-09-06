a64001498aa5e7fa4a77347a7cfbe134efaecb18
Laurent Thévenoux <lrnt@thvnx.com>
2024-09-06 21:25:11 +0200

    fixup! fixup! fixup! Use packaged version of mpfr

--- src/config/discover.ml
+++ src/config/discover.ml
@@ -7,14 +7,14 @@ let () =
       let default : C.Pkg_config.package_conf =
         { libs = [ "-lmpfr" ]; cflags }
       in
-      let conf =
+      let conf p =
         match C.Pkg_config.get c with
         | None -> default
         | Some pc -> (
-            match C.Pkg_config.query pc ~package:"mpfr" with
+            match C.Pkg_config.query pc ~package:p with
             | None -> default
             | Some deps -> deps)
       in
 
-      C.Flags.write_sexp "c_flags.sexp" (List.concat [ cflags; conf.cflags ]);
-      C.Flags.write_sexp "c_library_flags.sexp" conf.libs)
+      C.Flags.write_sexp "c_flags.sexp" (List.concat [ cflags; (conf "mpfr").cflags; (conf "gmp").cflags ]);
+      C.Flags.write_sexp "c_library_flags.sexp" (conf "mpfr").libs)
