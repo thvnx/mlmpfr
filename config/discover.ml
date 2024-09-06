59068290e0b545056818575a126b602c48ed4af1
Laurent Thévenoux <lrnt@thvnx.com>
2024-09-06 20:42:35 +0200

    Reorganize dune files for better C flags configuration

--- config/discover.ml
+++ config/discover.ml
@@ -5,16 +5,17 @@ let cflags = [ "-O2"; "-Wall"; "-Wextra"; "-fPIC" ]
 let () =
   C.main ~name:"discover-mpfr" (fun c ->
       let default : C.Pkg_config.package_conf =
-        { libs = [ "-lmpfr" ]; cflags }
+        { libs = [ "-lmpfr"; "-lgmp" ]; cflags }
       in
-      let conf =
+      let conf p =
         match C.Pkg_config.get c with
         | None -> default
         | Some pc -> (
-            match C.Pkg_config.query pc ~package:"mpfr" with
+            match C.Pkg_config.query pc ~package:p with
             | None -> default
             | Some deps -> deps)
       in
 
-      C.Flags.write_sexp "c_flags.sexp" (List.concat [ cflags; conf.cflags ]);
-      C.Flags.write_sexp "c_library_flags.sexp" conf.libs)
+      C.Flags.write_sexp "c_flags.sexp" (List.concat [ cflags; (conf "mpfr").cflags; (conf "gmp").cflags ]);
+      C.Flags.write_sexp "c_library_flags.sexp" (List.concat [ (conf "mpfr").libs; (conf "gmp").libs ])
+  )
