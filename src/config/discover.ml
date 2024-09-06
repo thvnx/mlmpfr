b1c00d591d24babfa20e158b592c5ec6d85d0cbf
Laurent Thévenoux <lrnt@thvnx.com>
2024-09-06 21:40:57 +0200

    fixup! fixup! fixup! fixup! Use packaged version of mpfr

--- src/config/discover.ml
+++ src/config/discover.ml
@@ -16,5 +16,6 @@ let () =
             | Some deps -> deps)
       in
 
-      C.Flags.write_sexp "c_flags.sexp" (List.concat [ cflags; (conf "mpfr").cflags; (conf "gmp").cflags ]);
-      C.Flags.write_sexp "c_library_flags.sexp" (conf "mpfr").libs)
+      C.Flags.write_sexp "c_flags.sexp" (List.concat [ cflags; (conf "mpfr").cflags; (conf "mpfr").libs ]);
+      C.Flags.write_sexp "c_library_flags.sexp" (List.concat [ (conf "mpfr").libs; (conf "gmp").libs ])
+  )
